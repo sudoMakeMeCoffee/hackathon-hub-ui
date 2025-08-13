2c2fa46d25599d7d0ba683d4d97920a917e5da82
sith <skdev.techno@gmail.com>
2025-08-13 18:44:55 +0530

    feat: add react router in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
