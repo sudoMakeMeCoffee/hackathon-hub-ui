bef45ab973d2344004c770ea0c8285dbaaa38ca1
sith <skdev.techno@gmail.com>
2025-08-16 11:41:07 +0530

    feat: implement logout

--- src/components/DashboardNav.jsx
+++ src/components/DashboardNav.jsx
@@ -2,12 +2,28 @@ import { Link, useLocation } from "react-router-dom";
 import { CgAddR } from "react-icons/cg";
 import { MdAddTask, MdLogout, MdOutlineDashboard } from "react-icons/md";
 import { RiUserAddLine } from "react-icons/ri";
+import axios from "axios";
 
 const DashboardNav = () => {
   const location = useLocation();
 
   // Utility to check if route is active
   const isActive = (path) => location.pathname === path;
+
+  const logout = () => {
+    axios
+      .post(
+        "http://localhost:8080/api/v1/auth/logout",
+        {},
+        { withCredentials: true }
+      )
+      .then(() => {
+        window.location.href = "/";
+      })
+      .catch((err) => {
+        console.error("Logout failed:", err);
+      });
+  };
 
   return (
     <div className="fixed md:top-0 bottom-0 left-0 right-0 w-full md:w-[70px] p-4 h-[70px] md:h-screen z-100 bg-primary text-secondary  md:flex flex-row md:flex-col items-center justify-between gap-10">
@@ -35,7 +51,9 @@ const DashboardNav = () => {
         <Link to="/dashboard/posts">
           <CgAddR
             className={`text-2xl cursor-pointer transition-all ${
-              isActive("/dashboard/posts") ? "font-bold scale-110" : "opacity-70"
+              isActive("/dashboard/posts")
+                ? "font-bold scale-110"
+                : "opacity-70"
             }`}
           />
         </Link>
@@ -51,7 +69,7 @@ const DashboardNav = () => {
         </Link>
       </div>
 
-      <div className="hidden md:flex ">
+      <div className="hidden md:flex" onClick={logout}>
         <MdLogout className="text-2xl cursor-pointer" />
       </div>
     </div>
