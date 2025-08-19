7d993633e893ff72fa4a7734f6942a79db3acc1b
sith <skdev.techno@gmail.com>
2025-08-19 15:49:16 +0530

    feat: make dashboard top nav responsive

--- src/components/dashboard/DashboardTopBar.jsx
+++ src/components/dashboard/DashboardTopBar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import logo from "../../assets/logo-dark.svg";
+import { BiSearch } from "react-icons/bi";
 
 const DashboardTopBar = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 bg-secondary w-full h-[70px] flex items-center border-b shadow-sm border-gray-200 z-40">
+    <div className="fixed top-0 left-0 right-0 bg-secondary w-full h-[70px]  flex items-center border-b shadow-sm border-gray-200 z-40">
       <div className="wrapper w-full flex items-center justify-between">
         <img src={logo} alt="" className="w-[110px] " />
 
@@ -11,9 +12,12 @@ const DashboardTopBar = () => {
           <input
             type="search"
             placeholder="Search"
-            className="border border-gray-200 text-sm rounded-md px-3 py-2 focus:outline-none"
+            className="border border-gray-200 text-sm rounded-md px-3 py-2 focus:outline-none hidden md:block"
           />
-          <button className="text-sm bg-gray-200 px-3 py-2 rounded-md">
+
+          <BiSearch className="text-xl block md:hidden cursor-pointer"/>
+
+          <button className="text-sm bg-gray-200 px-3 py-2 rounded-md hidden md:block">
             Logout
           </button>
 
